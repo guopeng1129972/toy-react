957d1af04e64fab2baa7e908e9f7438f5195bbef
guopeng1129972 <381694179@qq.com>
2020-09-03 00:09:57 +0800

    添加可以更新state的方法

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 import {createElement,Component,rander} from './toy-react'
 class MyComponent extends Component{
+  constructor(){
+    super()
+    this.state={a:1,b:2}
+  }
   rander(){
     return <div><h1>My component</h1>
-      {this.children}
+      <button onclick={()=>{this.setState({a:this.state.a+1})}}>add</button>
+      <span>{this.state.a.toString()}</span>
+      <span>{this.state.b.toString()}</span>
+      
     </div>
   }
 }
 
 
 
-rander(<MyComponent id='a' class="c">
+rander(<MyComponent id="a" class="c">
 <div>123</div>
 <div></div>
 <div></div>
--- toy-react.js
+++ toy-react.js
@@ -1,19 +1,37 @@
+const RENDER_TO_DOM = Symbol('rander_to_dom')
+
 class ElementWrapper {
   constructor(type) {
     this.root = document.createElement(type)
   }
   setAttribute(name, value) {
-    this.root.setAttribute(name, value)
+    if (name.match(/^on([\s\S]+)$/)) {
+      this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLocaleLowerCase()), value)
+    } else {
+      this.root.setAttribute(name, value)
+    }
   }
   appendChild(component) {
-    this.root.appendChild(component.root)
-  }
+      let range = document.createRange()
+      range.setStart(this.root, this.root.childNodes.length)
+      range.setEnd(this.root, this.root.childNodes.length)
+      component[RENDER_TO_DOM](range)
+    }
+    [RENDER_TO_DOM](range) {
+      range.deleteContents()
+      range.insertNode(this.root)
+    }
+
 }
 
 class TextWrapper {
   constructor(content) {
-    this.root = document.createTextNode(content)
-  }
+      this.root = document.createTextNode(content)
+    }
+    [RENDER_TO_DOM](range) {
+      range.deleteContents();
+      range.insertNode(this.root);
+    }
 
 }
 
@@ -21,18 +39,44 @@ export class Component {
   constructor() {
     this.props = Object.create(null)
     this.children = [];
+    this._root = null;
+    this._range = null;
   }
   setAttribute(name, value) {
     this.props[name] = value;
   }
   appendChild(component) {
-    this.children.push(component)
-  }
-  get root() {
-    if (!this._root) {
-      this._root = this.rander().root;
+      this.children.push(component)
     }
-    return this._root
+    [RENDER_TO_DOM](range) {
+      this._range = range;
+      this.rander()[RENDER_TO_DOM](range);
+    }
+  rerender() {
+    this._range.deleteContents();
+    this[RENDER_TO_DOM](this._range)
+  }
+  setState(newState) {
+    if (this.state === null || typeof this.state !== 'object') {
+      this.state = newState
+      this.rerender()
+      return
+    }
+    let merge = (oldState, newState) => {
+
+      for (let i in newState) {
+        if (typeof oldState[i] === null || typeof oldState[i] !== 'object') {
+          oldState[i]=newState[i]
+        }else{
+          merge(oldState[i],newState[i])
+        }
+
+      }
+
+    }
+    merge(this.state, newState)
+    this.rerender()
+
   }
 }
 
@@ -64,5 +108,9 @@ export function createElement(type, attributes, ...children) {
 }
 
 export function rander(component, parentElement) {
-  parentElement.appendChild(component.root)
+  let range = document.createRange()
+  range.setStart(parentElement, 0)
+  range.setEnd(parentElement, parentElement.childNodes.length)
+  range.deleteContents()
+  component[RENDER_TO_DOM](range)
 }
